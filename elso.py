138d56b23d74b91aff03742fd706b4ce5fdd380c
bbalint97 <berczi.balint97@gmail.com>
2025-09-13 15:20:31 +0200

    Elso bejegyzes

---
+++ elso.py
@@ -0,0 +1,18 @@
+
+a = 10
+b = 3
+
+print("Operatorok teszt")
+print("a:", a)
+print("b:", b)
+
+print("a+b:", a + b)
+print("a-b:", a - b)
+print("a*b:", a * b)
+print("a/b:", a / b)
+
+print("Szöveg:")
+szoveg = "teszt szoveg"
+print(szoveg)
+
+print("Hossza:", len(szoveg), "karatkter (space-el együtt)")
