2bd331f93b595693fbf9aec311e6c93c1f7f06a8
bbalint97 <berczi.balint97@gmail.com>
2025-09-28 21:28:50 +0200

    Első Labor kiegészítés

--- masodik.py
+++ masodik.py
@@ -1,9 +1,19 @@
 import turtle
 
+screen = turtle.Screen()
+screen.bgcolor("black")
+
 t = turtle.Turtle()
+t.color("white")
 
 for i in range(5):
     t.forward(200)
     t.right(144)
+
+def kilep():
+    screen.bye()
+
+screen.listen()
+screen.onkey(kilep, "q")
 
 turtle.done()
